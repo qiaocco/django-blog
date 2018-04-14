c1375d40136231b58a75de8be56a9ffccfb05956
jason <jasonqiao36@gmail.com>
2018-04-14 16:19:03 +0800

    add blog module models code

---
+++ django_blog/settings/develop.py
@@ -0,0 +1,15 @@
+from .base import *
+
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['jasonqiao36.cc']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
