a61654f3feeb964e71c0ed1c474c1dd0ef12792e
jason <jasonqiao36@gmail.com>
2018-04-21 11:10:29 +0800

    base config for admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, Category, Tag
+
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
+from django.db import connection
+from django.db.models.functions import Lower
 from django.test import TestCase
+from django.test.utils import override_settings
 
-# Create your tests here.
+from .models import Category, Post
+
+
+class TestCategory(TestCase):
+    def setUp(self):
+        user = User.objects.create_user('jason', 'wuxia64@qq.com', 'password')
+        for i in range(10):
+            category_name = 'Cat_{}'.format(i)
+            Category.objects.create(name=category_name, owner=user)
+
+    @override_settings(DEBUG=True)
+    def test_filter(self):
+        from django.db.models import Max, Count
+        categories = Category.objects.defer('name')
+        print(categories)
+        print(connection.queries)
