b3790a483af5ef11988a1b02512dabff3128a90e
jason <jasonqiao36@gmail.com>
2018-05-05 23:16:17 +0800

    add tag link

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ app_name = 'blog'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('category/<int:category_id>', CategoryView.as_view(), name='category'),
-    path('tag/<int:tag_id>', TagView.as_view(), name='post_list'),
+    path('tag/<int:tag_id>', TagView.as_view(), name='tag'),
     # path('links', views.links, name='links'),
     path('<int:pk>', PostView.as_view(), name='post_detail'),
 ]
