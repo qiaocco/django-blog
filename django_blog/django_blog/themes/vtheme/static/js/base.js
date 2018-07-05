f51542f0c7566574e08aced42c5a2cb6bc048e26
jason <jasonqiao36@gmail.com>
2018-07-05 19:42:30 +0800

    增加 返回顶部的小火箭

--- django_blog/django_blog/themes/vtheme/static/js/base.js
+++ django_blog/django_blog/themes/vtheme/static/js/base.js
@@ -1,13 +1,28 @@
-$(document).ready(function() {
-	//初始窗体尺寸，监控窗体变化事件
-	$(window).resize();
+$(document).ready(function () {
+    //初始窗体尺寸，监控窗体变化事件
+    $(window).resize();
 });
-$(window).resize(function() {
-	var bodyHeight = $(document.body).height();
-	var winHeight = $(window).height();
-	if (bodyHeight <= winHeight) {
-		$('.vc-footer').addClass('vc-fixed');
-	} else {
-		$('.vc-footer').removeClass('vc-fixed');
-	}
+$(window).resize(function () {
+    var bodyHeight = $(document.body).height();
+    var winHeight = $(window).height();
+    if (bodyHeight <= winHeight) {
+        $('.vc-footer').addClass('vc-fixed');
+    } else {
+        $('.vc-footer').removeClass('vc-fixed');
+    }
+});
+
+//GO TOP
+$(function () {
+    showScroll();
+
+    function showScroll() {
+        $(window).scroll(function () {
+            var scrollValue = $(window).scrollTop();
+            scrollValue > 100 ? $('div[class=backtop]').fadeIn() : $('div[class=backtop]').fadeOut();
+        });
+        $('div[class=backtop]').click(function () {
+            $("html,body").animate({scrollTop: 0}, 200);
+        });
+    }
 });
